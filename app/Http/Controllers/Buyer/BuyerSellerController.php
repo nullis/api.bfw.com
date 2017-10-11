95e73397ef9371094f377bc07fa279a8a974bda3
nullis <alex.wanjun@gmail.com>
2017-10-11 22:40:14 +0900

    Implementing Complex Operations for Category,Buyer,Seller

---
+++ app/Http/Controllers/Buyer/BuyerSellerController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\Buyer;
+
+use App\Buyer;
+use Illuminate\Http\Request;
+use App\Http\Controllers\ApiController;
+
+class BuyerSellerController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @param Buyer $buyer
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Buyer $buyer)
+    {
+        $sellers = $buyer->transactions()
+            ->with('product.seller')
+            ->get()
+            ->pluck('product.seller')
+            ->unique('id')
+            ->values();
+
+        return $this->showAll($sellers);
+    }
+}
